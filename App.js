2039402553b463a1ef7c902433a7ab103d1097a6
adnan haider <adnan.haider@ropstam.com>
2023-07-19 05:47:20 -0700

    resolved

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native'
+import {NavigationContainer} from '@react-navigation/native';
 import AppNavigator from './scr/navigation/AppNavigator';
-import { Provider as StoreProvider } from "react-redux";
-import store from './scr/redux/store'
+import {Provider as StoreProvider} from 'react-redux';
+import store from './scr/redux/store';
 const App = () => {
   return (
     <StoreProvider store={store}>
@@ -10,7 +10,6 @@ const App = () => {
         <AppNavigator />
       </NavigationContainer>
     </StoreProvider>
-
   );
-}
+};
 export default App;
--- scr/navigation/AppNavigator.js
+++ scr/navigation/AppNavigator.js
@@ -1,32 +1,29 @@
-import React from 'react'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import SignIn from '../screens/SignIn';
 import SignUp from '../screens/SignUp';
 import Dashboard from '../screens/Dashboard';
 import Car from '../screens/Car';
 import CarAdd from '../screens/CarAdd';
 
-
 const Stack = createNativeStackNavigator();
 const AppNavigator = () => {
-    return (
-        <Stack.Navigator initialRouteName='SignIn'>
-            <Stack.Screen
-                name="SignIn"
-                component={SignIn}
-                options={{ headerShown: false }}
-            />
-            <Stack.Screen
-                name="SignUp"
-                component={SignUp}
-                options={{ headerShown: false }}
-            />
-            <Stack.Screen name="Dashboard" component={Dashboard} />
-            <Stack.Screen name="Car" component={Car} />
-            <Stack.Screen name="CarAdd" component={CarAdd} />
-
-
-        </Stack.Navigator>
-    )
-}
+  return (
+    <Stack.Navigator initialRouteName="SignIn">
+      <Stack.Screen
+        name="SignIn"
+        component={SignIn}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="SignUp"
+        component={SignUp}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen name="Dashboard" component={Dashboard} />
+      <Stack.Screen name="Car" component={Car} />
+      <Stack.Screen name="CarAdd" component={CarAdd} />
+    </Stack.Navigator>
+  );
+};
 export default AppNavigator;
