1e0879d7df23fc34dd755553418c38e04a836b06
adnan haider <adnan.haider@ropstam.com>
2023-07-19 05:51:51 -0700

    resolved

--- scr/navigation/AppNavigator.js
+++ scr/navigation/AppNavigator.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createStackNavigator} from '@react-navigation/native-stack';
 import SignIn from '../screens/SignIn';
 import SignUp from '../screens/SignUp';
 import Dashboard from '../screens/Dashboard';
 import Car from '../screens/Car';
 import CarAdd from '../screens/CarAdd';
 
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
 const AppNavigator = () => {
   return (
     <Stack.Navigator initialRouteName="SignIn">
